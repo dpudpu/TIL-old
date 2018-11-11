d26e81ee7313a6bbc846ea6c9d62c9495c8f61e3
DaejunBae <dpudpu11@gmail.com>
2018-11-11 23:43:18 +0900

    Spring DATA JPA

---
+++ springboot/sunnyboard/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into sunny_member(id, name) values (1, 'kim');
+insert into sunny_board(content, member_id, read_count, reg_date, title, update_date) values ('content', 1, 0, now(), 'title', now());
+insert into sunny_board(content, member_id, read_count, reg_date, title, update_date) values ('content', 1, 0, now(), 'title', now());
+insert into sunny_board(content, member_id, read_count, reg_date, title, update_date) values ('content', 1, 0, now(), 'title', now());
