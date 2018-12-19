4b0d6ab35482a8bb08b9903ae12311b2c1bee3ea
DaejunBae <dpudpu11@gmail.com>
2018-12-19 15:46:39 +0700

    Add Quiz4811

---
+++ algorithm/src/algorithm/backjoon/recursion/Quiz4811.java
@@ -0,0 +1,28 @@
+package algorithm.backjoon.recursion;
+
+import java.util.Scanner;
+
+public class Quiz4811 {
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+
+        int N;
+        while ((N = sc.nextInt()) != 0) {
+            int w = 0;
+            int h = 0;
+
+            recursion(w, h);
+
+        }
+
+    }
+
+    public static void recursion(int w, int h) {
+
+    }
+
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
