8efea1fc97c1a9f1267b603b997ae894e41ac297
DaejunBae <dpudpu11@gmail.com>
2019-03-13 14:08:19 +0900

    Add LeetCode300

---
+++ algorithm/src/algorithm/leetcode/LeetCode300.java
@@ -0,0 +1,24 @@
+package algorithm.leetcode;
+
+public class LeetCode300 {
+    public static int lengthOfLIS(int[] nums) {
+        int dp[] = new int[nums.length];
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    max = Math.max(dp[j], max);
+                }
+            }
+            dp[i] = max + 1;
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println(lengthOfLIS(nums));
+    }
+}
