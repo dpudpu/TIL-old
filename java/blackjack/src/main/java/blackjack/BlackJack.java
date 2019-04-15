976541c992329e705671e8d654cb6920225e4bfc
DaejunBae <dpudpu11@gmail.com>
2019-04-15 17:32:09 +0900

    Add 카드 구현 (문양, 숫자는 enum으로)

---
+++ java/blackjack/src/main/java/blackjack/BlackJack.java
@@ -0,0 +1,9 @@
+package blackjack;
+
+import blackjack.domain.CardDeck;
+
+public class BlackJack {
+    public void run() {
+        CardDeck cardDeck = new CardDeck();
+    }
+}
---
+++ java/blackjack/src/main/java/blackjack/Main.java
@@ -0,0 +1,8 @@
+package blackjack;
+
+public class Main {
+    public static void main(String[] args) {
+        BlackJack blackJack = new BlackJack();
+        blackJack.run();
+    }
+}
