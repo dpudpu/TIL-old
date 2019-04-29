dee0923c8164724c41d098309cbc41426c23c799
DaejunBae <dpudpu11@gmail.com>
2019-04-29 16:04:13 +0900

    Delete Blackjack

--- java/blackjack/src/main/java/blackjack/BlackJack.java
+++
@@ -1,9 +0,0 @@
-package blackjack;
-
-import blackjack.domain.CardDeck;
-
-public class BlackJack {
-    public void run() {
-        CardDeck cardDeck = new CardDeck();
-    }
-}
--- java/blackjack/src/main/java/blackjack/Main.java
+++
@@ -1,8 +0,0 @@
-package blackjack;
-
-public class Main {
-    public static void main(String[] args) {
-        BlackJack blackJack = new BlackJack();
-        blackJack.run();
-    }
-}
