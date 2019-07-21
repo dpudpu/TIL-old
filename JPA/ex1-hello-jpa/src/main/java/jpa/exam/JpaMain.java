b2a4e3ee991bbbf7cae2b5c3022179df528af966
DaejunBae <dpudpu11@gmail.com>
2019-07-21 21:26:32 +0900

    jpa 예제 추

--- JPA/ex1-hello-jpa/src/main/java/jpa/exam/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/jpa/exam/JpaMain.java
@@ -20,10 +20,21 @@ public class JpaMain {
             member.setId(1L);
             member.setName("foo");
 
+            System.out.println("--------------before persist");
             //영속
             em.persist(member);
+            System.out.println("--------------after persist");
 
+            // 준영속 jpa에서 더이상 관리하지 않아서 디비에 커밋이 안된다.
+            em.detach(member);
+
+            System.out.println("--------------before flush");
+            em.flush();
+            System.out.println("--------------after flush");
+
+            System.out.println("-----------before update");
             member.setName("Asd");
+            System.out.println("-----------after update");
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
