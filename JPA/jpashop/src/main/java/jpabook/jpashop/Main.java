557792efb0a78c0c535e6d6714e8a315214addf4
DaejunBae <dpudpu11@gmail.com>
2019-07-25 14:20:52 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- JPA/jpashop/src/main/java/jpabook/jpashop/Main.java
+++ JPA/jpashop/src/main/java/jpabook/jpashop/Main.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,11 +17,11 @@ public class Main {
         tx.begin();
         try {
 
-            Order order = em.find(Order.class, 1L);
-            Long memberId = order.getMemberId();
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
 
 
-
+            tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback();
--- JPA/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -12,8 +14,12 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
@@ -28,12 +34,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(final Long memberId) {
-        this.memberId = memberId;
+    public void setMember(final Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
@@ -50,5 +56,10 @@ public class Order {
 
     public void setStatus(final OrderStatus status) {
         this.status = status;
+    }
+
+    public void addOrderItem(final OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
     }
 }
