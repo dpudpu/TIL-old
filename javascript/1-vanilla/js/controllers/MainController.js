39f67f7d9f5ef0faef65cc2b0971f4ea218e833f
DaejunBae <dpudpu11@gmail.com>
2019-08-18 11:39:36 +0900

    js 탭구현

--- javascript/1-vanilla/js/controllers/MainController.js
+++ javascript/1-vanilla/js/controllers/MainController.js
@@ -1,8 +1,10 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
+import TabView from '../Views/TabView.js'
 
+import HistoryModel from '../models/HistoryModel.js'
+import KeywordModel from '../models/KeywordModel.js'
 import SearchModel from '../models/SearchModel.js'
-
 
 const tag = '[MainController]'
 
@@ -13,7 +15,20 @@ export default {
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm())
 
+        TabView.setup(document.querySelector('#tabs'))
+            .on('@change', e => this.onChangeTab(e.detail.tabName))
+
         ResultView.setup(document.querySelector("#search-result"))
+
+        this.selectedTab = '최근 검색어'
+        this.renderView()
+    },
+
+    renderView() {
+        console.log(tag, 'renderView()')
+        TabView.setActiveTab(this.selectedTab);
+
+        ResultView.hide()
     },
 
     search(query) {
@@ -35,5 +50,9 @@ export default {
 
     onSearchResult(data) {
         ResultView.render(data)
+    },
+
+    onChangeTab(tabName) {
+    debugger
     }
 }
